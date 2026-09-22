e59950a45ba4dfec732558f0aff89f52531d1c32
finmohona-netizen <finmohona@gmail.com>
2026-09-22 22:53:08 +0300

    Add W2 folder with files

---
+++ W2/A2_T2.py
@@ -0,0 +1,5 @@
+print("Program starting.")
+Brand=input(" Insert car brand:")
+Model=input(" Insert car model:")
+print("Car brand is", f'"{Brand}"' ,"and the model is" , f"'{Model}'.")
+print("Program ending.")
